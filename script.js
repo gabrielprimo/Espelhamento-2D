6e84487c36c66d07d931e214440de66e15325fec
Glitch (hello-webpage) <none>
2020-02-11 17:30:41 +0000

    🍴🐝 Checkpoint ./index.html:6462618/299 ./script.js:6462618/916

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+  function setPixel(imageData, x, y, r, g, b, a) {
+    var index = (x + y * imageData.width) * 4;
+    imageData.data[index+0] = r;
+    imageData.data[index+1] = g;
+  	imageData.data[index+2] = b;
+	  imageData.data[index+3] = a;
+  }    
+
+  function draw() {	
+	  var canvas = document.getElementById("tutorial");
+    var ctx = canvas.getContext("2d");
+	
+	  ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(10,10,55,50); 
+
+	  ctx.moveTo(100,20);
+	  ctx.lineTo(100,27);
+	  ctx.stroke();
+  }
+
+  function draw_line() {
+	  var canvas = document.getElementById("tutorial");
+    var ctx = canvas.getContext("2d");
+    var canvasData = ctx.createImageData(300,300);
+
+	  for(var x=0; x<300; x++) {
+		  setPixel(canvasData,x,200,200,255,0,255);	
+	  }
+
+	  ctx.putImageData(canvasData, 0, 0);
+  }
