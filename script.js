a9ee9fc13196d67f2d494beb96fa3f69feb1d8f4
Glitch (hello-webpage) <none>
2020-03-17 01:01:22 +0000

    🚟⛷ Checkpoint ./script.js:6462618/644 ./index.html:6462618/15

--- script.js
+++ script.js
@@ -5,21 +5,19 @@
   var forma=[];
   forma= Array(2);
 
+  //3 pontos que formam um triangulo são colocados no array forma
   forma[0] = (100,100);
   forma[1] = (100,200);
   forma[2] = (200,100);
 
-  //desenha o poligono a partir dos pontos passada
+  //desenha o poligono a partir dos pontos colocados no array forma
   function draw_polygon() {	
 	  var canvas = document.getElementById("tutorial");
     var ctx = canvas.getContext("2d");
-	
-	  ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(10,10,55,50); 
-
-	  ctx.moveTo(100,20);
-	  ctx.lineTo(100,27);
-	  ctx.stroke();
+	  //window.alert(forma[2]);
+    for(var i=0; i< forma.length; i++) {
+        draw_line(forma[i][0],forma[i][1]);
+    }
   }
 
   //função que desenha uma reta a partir de 2 pontos
