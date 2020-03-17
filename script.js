b8e6d4a2f8b2a64e724c2ac399e5f202f8ae2174
Glitch (hello-webpage) <none>
2020-03-17 00:50:21 +0000

    🍀📖 Checkpoint ./script.js:6462618/551

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-  function draw() {	
+  //criação de um array bi-dimensional
+  var forma=[];
+  forma= Array(2);
+
+  forma[0] = (100,100);
+  forma[1] = (100,200);
+  forma[2] = (200,100);
+
+  //desenha o poligono a partir dos pontos passada
+  function draw_polygon() {	
 	  var canvas = document.getElementById("tutorial");
     var ctx = canvas.getContext("2d");
 	
@@ -13,12 +22,13 @@
 	  ctx.stroke();
   }
 
+  //função que desenha uma reta a partir de 2 pontos
   function draw_line(p1, p2) {
 	  var canvas = document.getElementById("tutorial");
     var ctx = canvas.getContext("2d");
     var canvasData = ctx.createImageData(300,300);
 
 	  ctx.moveTo(p1[0],p1[1]);
-
-	  ctx.putImageData(canvasData, 0, 0);
+    ctx.lintTo(p2[0],p2[1]);
+    ctx.stroke();
   }
